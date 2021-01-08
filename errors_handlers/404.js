d03188395be75ed0b02f2f8d196a95d5cd37203f
Vitalii <ghvitaliidrapaliuk@gmail.com>
2021-01-08 12:18:11 +0200

    fix 404 handlers

--- errors_handlers/404.js
+++ errors_handlers/404.js
@@ -2,7 +2,7 @@ const ResponseError = require('./response_error')
 
 const notFound = (req, res, next) => {
     console.log('MY PERSONAL 404')
-    next(createError(new ResponseError('NOT FOUND', 404, 'request resource not found')))
+    next(new ResponseError('NOT FOUND', 404, 'request resource not found'))
 }
 
 module.exports = notFound;
