9d5ae7c3ec027c10c4f362ccdc743a8b679ddabe
Vitalii <ghvitaliidrapaliuk@gmail.com>
2021-04-17 18:23:52 +0300

    change mail service

--- utils/mail.js
+++ utils/mail.js
@@ -1,31 +1,70 @@
-const nodemailer = require('nodemailer');
-const smtpConfig = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // use SSL
-  auth: {
-    user: 'valiakusil1945@gmail.com',
-    pass: '19452507Viktor',
-  },
+// const nodemailer = require('nodemailer');
+// const sgTransport = require('nodemailer-sendgrid-transport')
+// const smtpConfig = {
+//   host: 'smtp.gmail.com',
+//   port: 465,
+//   secure: true, // use SSL
+//   auth: {
+//     user: 'valiakusil1945@gmail.com',
+//     pass: '19452507Viktor',
+//   },
+// };
+
+// const ADMIN_EMAIL_API_KEY = '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'
+
+// // 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
+// // const transporter = nodemailer.createTransport(smtpConfig);
+
+// const transporter = nodemailer.createTransport(sgTransport({
+//   auth: {
+//       api_user:
+//       api_key: ADMIN_EMAIL_API_KEY // your api key here, better hide it in env vars
+//   }
+// }))
+
+// const mailer = async (to, subject, text) => {
+//   const options = {
+//     from: 'valiakusil1945@gmail.com',
+//     // to: 'vitaliidrapaliuk@gmail.com',
+//     to,
+//     subject,
+//     text,
+//   };
+
+//   return await transporter.sendMail(options);
+// };
+// exports.sendEmail = async (email, subject, data) => {
+//   let result = { err: null };
+
+//   try {
+//     await mailer(email, subject, data);
+//   } catch (error) {
+//       console.log(error)
+//     return result;
+//   }
+
+//   return result;
+// };
+
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey('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')
+
+const msg = {
+  to: 'vitaliidrapaliuk@gmail.com',
+  from: 'valiakusil1945@gmail.com', // Use the email address or domain you verified above
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 
-const transporter = nodemailer.createTransport(smtpConfig);
-const mailer = async (to, subject, text) => {
-  const options = {
-    from: 'valiakusil1945@gmail.com',
-    // to: 'vitaliidrapaliuk@gmail.com',
-    to,
-    subject,
-    text,
-  };
 
-  return await transporter.sendMail(options);
-};
+
 exports.sendEmail = async (email, subject, data) => {
   let result = { err: null };
 
   try {
-    await mailer(email, subject, data);
+    await sgMail.send(msg);
   } catch (error) {
       console.log(error)
     return result;
