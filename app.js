6c2b8b67f15b1a63778d53d9a2444b89f206913f
Vitalii <ghvitaliidrapaliuk@gmail.com>
2021-04-27 16:42:13 +0300

    adas

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const isAuthorization = require('./middlewares/is_authorizathison');
+const authRouter = require('./routes/auth/router')
+const initializeRouter = require('./routes/initialize/router');
+const listsRouter = require('./routes/lists/router');
+const tasksRouter = require('./routes/tasks/router');
+const biographyRouter = require('./routes/biography/router');
+const settingsRouter = require('./routes/settings/router');
+const personalDataRouter = require('./routes/personal_data/router');
+const defaultListsRouter = require('./routes/default_tasks_lists/router');
+const listFoldersRouter = require('./routes/folders/router')
+const errorHandler = require('./errors_handlers/errors_handler');
+const notFound = require('./errors_handlers/404');
+const { sendEmail } = require('./utils/mail');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
+app.use(cors(corsOptions));
+app.use('/test', async (req, res) => {
+  const result = await sendEmail('vitaliidrapaliuk@gmail.com', 'Working', 'Text')
+  res.json(result)
+})
+app.use('/auth', authRouter)
+app.use('/', isAuthorization)
+app.use('/initialize', initializeRouter)
+app.use('/initialize/check-out', initializeRouter) //?
+app.use('/tasks', tasksRouter)
+app.use('/lists', listsRouter)
+app.use('/settings', settingsRouter)
+app.use('/biography', biographyRouter)
+app.use('/personalData', personalDataRouter)
+app.use('/default-lists', defaultListsRouter)
+app.use('/folders', listFoldersRouter)
+
 const token = '1776151137:AAFqW5T8_5QSqK54a_77LSX6wmFmHkyxXpA'
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {
@@ -14,64 +67,7 @@ bot.on('message', (msg) => {
     bot.sendMessage(msg.chat.id, 'First attempt!')
 })
 
+app.use(notFound);
+app.use(errorHandler)
 
-
-// const express = require('express');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
-// const cors = require('cors');
-
-// const isAuthorization = require('./middlewares/is_authorizathison');
-// const authRouter = require('./routes/auth/router')
-// const initializeRouter = require('./routes/initialize/router');
-// const listsRouter = require('./routes/lists/router');
-// const tasksRouter = require('./routes/tasks/router');
-// const biographyRouter = require('./routes/biography/router');
-// const settingsRouter = require('./routes/settings/router');
-// const personalDataRouter = require('./routes/personal_data/router');
-// const defaultListsRouter = require('./routes/default_tasks_lists/router');
-// const listFoldersRouter = require('./routes/folders/router')
-// const errorHandler = require('./errors_handlers/errors_handler');
-// const notFound = require('./errors_handlers/404');
-// const { sendEmail } = require('./utils/mail');
-// const app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({
-//   extended: false
-// }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// const corsOptions = {
-//   "origin": "*",
-//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   "preflightContinue": false,
-//   "optionsSuccessStatus": 204
-// }
-
-// app.use(cors(corsOptions));
-// app.use('/test', async (req, res) => {
-//   const result = await sendEmail('vitaliidrapaliuk@gmail.com', 'Working', 'Text')
-//   res.json(result)
-// })
-// app.use('/auth', authRouter)
-// app.use('/', isAuthorization)
-// app.use('/initialize', initializeRouter)
-// app.use('/initialize/check-out', initializeRouter) //?
-// app.use('/tasks', tasksRouter)
-// app.use('/lists', listsRouter)
-// app.use('/settings', settingsRouter)
-// app.use('/biography', biographyRouter)
-// app.use('/personalData', personalDataRouter)
-// app.use('/default-lists', defaultListsRouter)
-// app.use('/folders', listFoldersRouter)
-
-
-
-// app.use(notFound);
-// app.use(errorHandler)
-
-// module.exports = app;
+module.exports = app;
